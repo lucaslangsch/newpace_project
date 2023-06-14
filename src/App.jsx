06a686d1b950924bd3e35d22305a398320ec3239
lucaslangsch <lucas.langsch@gmail.com>
2023-06-13 21:37:45 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Route, Switch } from 'react-router-dom';
+import './App.css';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <Switch>
+      <Route exact path="/" component={ Home } />
+    </Switch>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <main>
+        <h1>Hello world!</h1>
+      </main>
+    );
+  }
+}
+
+export default Home;
