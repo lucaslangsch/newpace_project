905c2814f20cd3e4b2a3b2c067199eba795e3936
lucaslangsch <lucas.langsch@gmail.com>
2023-07-11 17:14:42 -0300

    remove console

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -27,6 +27,7 @@ function Header() {
         <a href="/">INÍCIO</a>
         <Link to="/planos">PLANOS</Link>
         <a href="/locais">LOCAIS DE TREINO</a>
+        <a href="locais">LOCAIS 2</a>
         <a target="_blank" href="https://www.instagram.com/newpaceassessoria/" rel="me"><AiOutlineInstagram /></a>
         <a target="_blank" href="https://wa.me/5548999408873?text=Ol%C3%A1,%20gostaria%20de%20saber%20mais%20sobre%20os%20planos%20da%20Newpace" rel="me"><AiOutlineWhatsApp /></a>
       </nav>
