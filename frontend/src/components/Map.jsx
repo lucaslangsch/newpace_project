ae2bfef7262d50f6521e8a61c188072dbfa1efd6
lucaslangsch <lucas.langsch@gmail.com>
2023-07-11 20:29:51 -0300

    feat: add chave api

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -14,7 +14,7 @@ class Map extends Component {
         {
           window.google === undefined ? 
             <LoadScript
-              googleMapsApiKey="AIzaSyBR97BnYUY7o1fZUo3vvKUv128Rk7fhSOU"
+              googleMapsApiKey={import.meta.env.VITE_API_GOOGLE_KEY}
             >
             <GoogleMap
               mapContainerStyle={containerStyle}
