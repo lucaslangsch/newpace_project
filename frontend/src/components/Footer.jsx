67227583ff0e41d644302b7a99f3ada6ab09b1a6
lucaslangsch <lucas.langsch@gmail.com>
2023-07-11 21:51:02 -0300

    fix: carddepo responsive

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -9,14 +9,15 @@ function Footer() {
       <div className="footer-left">
         <h2>FIQUE POR  DENTRO DO NOSSO <span className="strong-footer"> CONTEÚDO: </span></h2>
         <nav className="nav-footer-link">
-          <a className="link" href="https://www.instagram.com/newpaceassessoria/"><AiOutlineInstagram /></a>
-          <a className="link" href="https://wa.me/5548999408873?text=Ol%C3%A1,%20gostaria%20de%20saber%20mais%20sobre%20os%20planos%20da%20Newpace"><AiOutlineWhatsApp /></a>
+          <a className="link" target="_blank" href="https://www.instagram.com/newpaceassessoria/"><AiOutlineInstagram /></a>
+          <a className="link" target="_blank" href="https://wa.me/5548999408873?text=Ol%C3%A1,%20gostaria%20de%20saber%20mais%20sobre%20os%20planos%20da%20Newpace"><AiOutlineWhatsApp /></a>
         </nav>
       </div>
       <div className="footer-right">
         <h2>QUER SER NOSSO <span className="strong-footer"> PARCEIRO? </span></h2>
         <p>Conheça o clube NP, e venha integrar nossa rede técnica de empresas e profissionais.</p>
-        <a className="link-partners">saiba mais <span className="t">&gt;&gt;&gt;</span></a>
+        <a className="link-partners" target="_blank" href="https://wa.me/5548998544763?text=Ol%C3%A1,%20como%20fa%C3%A7o%20para%20ser%20um%20dos%20parceiros%20da%20Newpace%20Assessoria?">saiba mais <span className="t">&gt;&gt;&gt;</span></a>
+        <p>{import.meta.env.VITE_API_GOOGLE_KEY}</p>
       </div>
     </footer>
     </IconContext.Provider>
