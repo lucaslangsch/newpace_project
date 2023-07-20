ca9cdb16d9e82025cdfbac2dfcd417d88b5a0ab4
lucaslangsch <lucas.langsch@gmail.com>
2023-07-20 18:16:37 -0300

    fix: passing env api key directly

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -5,12 +5,10 @@ const containerStyle = {
   width: '100%',
   height: '100%'
 };
+const key = import.meta.env.VITE_API_GKEY   // Usa a chave de produção durante a fase de produção
 
 class Map extends Component {
   render() {
-    const key = import.meta.env.DEV
-    ? import.meta.env.VITE_API_GKEY // Usa a chave de desenvolvimento durante a fase de desenvolvimento
-    : process.env.VITE_API_GKEY;    // Usa a chave de produção durante a fase de produção
     console.log('meta.env: ',import.meta.env);
     const {location} = this.props;
     return (
